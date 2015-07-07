bea5aafe4c2aec0f7b89194e78f59652295fb2bd
Edward Ruchevits <ruchevits@gmail.com>
2015-07-07 17:40:07 +0200

    Documentation related Mongo collections

---
+++ collections/docs.js
@@ -0,0 +1,4 @@
+DocCompounds = new Mongo.Collection('DocCompounds');
+DocProjects = new Mongo.Collection('DocProjects');
+DocArticles = new Mongo.Collection('DocArticles');
+DocVersions = new Mongo.Collection('DocVersions');
