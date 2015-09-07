f367431f43f2f0601d342f096c7aa776824a15c7
Edward Ruchevits <ruchevits@gmail.com>
2015-09-07 18:33:35 +0100

    Displaying basic Doxygen compounds

--- client/modules/reference/directives/compound.directive.ng.js
+++ client/modules/reference/directives/compound.directive.ng.js
@@ -22,11 +22,212 @@ function compound($compile, $templateCache) {
                     element.append(createMarkdownCompounds(scope));
                     break;
 
+                case 'doxygen':
+                    element.append(createDoxygenCompounds(scope));
+                    break;
+
             }
 
         }
 
     };
+
+    function createDoxygenPage(scope) {
+
+        var $sections = $('<div></div>');
+
+        // TODO: sort compounds appropriately
+        var sortedCompounds = scope.body;
+
+        _.forEach(sortedCompounds, function(value, key){
+
+            var $section = $('<section class="compound"></section>');
+
+            switch(key) {
+
+                case 'name':
+                    scope.name = value;
+                    $section.addClass('compound-name');
+                    $section.append($compile(
+                        $templateCache.get('client/components/compounds/views/name.ng.html')
+                    )(scope));
+                    break;
+
+                default:
+                    console.log('No template for: ' + key);
+                    return;
+
+            }
+
+            $sections.append($section);
+
+        });
+
+        return $sections;
+
+    }
+
+    function createDoxygenFile(scope) {
+
+        var $sections = $('<div></div>');
+
+        // TODO: sort compounds appropriately
+        var sortedCompounds = scope.body;
+
+        _.forEach(sortedCompounds, function(value, key){
+
+            var $section = $('<section class="compound"></section>');
+
+            switch(key) {
+
+                case 'name':
+                    scope.name = value;
+                    $section.addClass('compound-name');
+                    $section.append($compile(
+                        $templateCache.get('client/components/compounds/views/name.ng.html')
+                    )(scope));
+                    break;
+
+                default:
+                    console.log('No template for: ' + key);
+                    return;
+
+            }
+
+            $sections.append($section);
+
+        });
+
+        return $sections;
+
+    }
+
+    // TODO: review efficiency
+    function sortCompounds(compounds, order){
+
+        var keys = Object.keys(compounds);
+        var i, len = keys.length;
+        var sortedCompounds = {};
+
+        keys.sort(function(a,b){
+            var indexA = $.inArray(a, order);
+            var indexB = $.inArray(b, order);
+            return (indexA < indexB) ? -1 : (indexA > indexB) ? 1 : 0;
+        });
+
+        for (i = 0; i < len; i++){
+            sortedCompounds[keys[i]] = compounds[keys[i]];
+        }
+
+        return sortedCompounds;
+
+    }
+
+    function createDoxygenClass(scope) {
+
+        var $sections = $('<div></div>');
+
+        var sortedCompounds = sortCompounds(scope.body, [
+            'name',
+            "kind",
+            "prot",
+            'briefdescription'
+        ]);
+
+        _.forEach(sortedCompounds, function(value, key){
+
+            var $section = $('<section class="compound"></section>');
+
+            switch(key) {
+
+                case 'name':
+                    scope.name = value;
+                    $section.addClass('compound-name');
+                    $section.append($compile(
+                        $templateCache.get('client/components/compounds/views/name.ng.html')
+                    )(scope));
+                    break;
+
+                case 'briefdescription':
+                    scope.summary = value;
+                    $section.addClass('compound-summary');
+                    $section.append($compile(
+                        $templateCache.get('client/components/compounds/views/summary.ng.html')
+                    )(scope));
+                    break;
+
+                case 'kind':
+                    // Do nothing
+                    break;
+
+                default:
+                    console.log('No template for: ' + key);
+                    return;
+
+            }
+
+            $sections.append($section);
+
+        });
+
+        return $sections;
+
+    }
+
+    function createDoxygenDir(scope) {
+
+        var $sections = $('<div></div>');
+
+        // TODO: sort compounds appropriately
+        var sortedCompounds = scope.body;
+
+        _.forEach(sortedCompounds, function(value, key){
+
+            var $section = $('<section class="compound"></section>');
+
+            switch(key) {
+
+                case 'name':
+                    scope.name = value;
+                    $section.addClass('compound-name');
+                    $section.append($compile(
+                        $templateCache.get('client/components/compounds/views/name.ng.html')
+                    )(scope));
+                    break;
+
+                default:
+                    console.log('No template for: ' + key);
+                    return;
+
+            }
+
+            $sections.append($section);
+
+        });
+
+        return $sections;
+
+    }
+
+    function createDoxygenCompounds(scope) {
+
+        switch (scope.body.kind) {
+
+            case 'page':
+                return createDoxygenPage(scope);
+
+            case 'file':
+                return createDoxygenFile(scope);
+
+            case 'class':
+                return createDoxygenClass(scope);
+
+            case 'dir':
+                return createDoxygenDir(scope);
+
+        }
+
+    }
 
     function createMarkdownCompounds(scope) {
 
